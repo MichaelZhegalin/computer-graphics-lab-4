ab4ec137c5ddd431ecd593f554375ba7980aef78
MichaelZhegalin <mixail.zhegalin@mail.ru>
2023-03-02 14:49:32 +0300

    Сделал функцию соприкосания окружностей более читаемой

--- src/functionsForTangents/CircleTangencyCheck.js
+++ src/functionsForTangents/CircleTangencyCheck.js
@@ -1,32 +1,39 @@
 export const CircleTangencyCheck = (circleOne, circleTwo, context, smallerCircle, largerCircle) => {
 
     let circleTangency = false;
+    let extensionCoeff = 1;
+    const circleOneY = Number(circleOne.yCoordinate);
+    const circleTwoY = Number(circleTwo.yCoordinate);
+    const circleOneX = Number(circleOne.xCoordinate);
+    const circleTwoX = Number(circleTwo.xCoordinate);
+    const radiusOne = Number(circleOne.radius);
+    const radiusTwo = Number(circleTwo.radius);
+    const smallerCircleX = Number(smallerCircle.xCoordinate);
+    const smallerCircleY = Number(smallerCircle.yCoordinate);
+    const smallerCircleRadius = Number(smallerCircle.radius);
+    const largerCircleX = Number(largerCircle.xCoordinate);
+    const largerCircleY = Number(largerCircle.yCoordinate);
 
-    if(circleOne.yCoordinate === circleTwo.yCoordinate && (Number(circleOne.radius) + Number(circleTwo.radius)) === Math.abs((Math.abs(circleOne.xCoordinate) - Math.abs(circleTwo.xCoordinate)))){
+    if(circleOneY === circleTwoY && (radiusOne + radiusTwo) === Math.abs((Math.abs(circleOneX) - Math.abs(circleTwoX)))){
         circleTangency = true;
         context.beginPath()
         context.strokeStyle = 'blue'
         context.lineWidth = 2;
 
-        let x2 = Number(smallerCircle.xCoordinate) - Number(smallerCircle.radius)
-        let x1 = Number(smallerCircle.xCoordinate) + Number(smallerCircle.radius)
-        let govno = 1;
+        let x2 = smallerCircleX - smallerCircleRadius
+        let x1 = smallerCircleX + smallerCircleRadius
+        extensionCoeff = 1;
 
-        if(Math.sqrt((x2 - Number(largerCircle.xCoordinate))**2 + (0 - Number(largerCircle.xCoordinate)) ** 2) >= Math.sqrt((x1 - Number(largerCircle.xCoordinate))**2 + (0 - Number(largerCircle.xCoordinate)) ** 2)){
-            console.log("Влево")
-            console.log("Координата меньшей окружности:", smallerCircle)
-            govno = 1;
-
+        if(Math.sqrt((x2 - largerCircleX)**2 + (0 - largerCircleX) ** 2) >= Math.sqrt((x1 - largerCircleX)**2 + (0 - largerCircleX) ** 2)){
+            extensionCoeff = 1;
         }
         else{
-            console.log("Вправо")
-            console.log("Координата меньшей окружности:", smallerCircle)
-            govno = -1
+            extensionCoeff = -1
         }
-        context.moveTo((Number(smallerCircle.yCoordinate) + 10) * 10 , (Number(smallerCircle.xCoordinate) + Number(smallerCircle.radius) * govno) * 10)
-        context.lineTo((Number(smallerCircle.yCoordinate) - 10) * 10, (Number(smallerCircle.xCoordinate) + Number(smallerCircle.radius) * govno) * 10)
+        context.moveTo((smallerCircleY + 10) * 10 , (smallerCircleX + smallerCircleRadius * extensionCoeff) * 10)
+        context.lineTo((smallerCircleY - 10) * 10, (smallerCircleX + smallerCircleRadius * extensionCoeff) * 10)
         context.stroke()
-    }else if(circleOne.xCoordinate === circleTwo.xCoordinate && (Number(circleOne.radius) + Number(circleTwo.radius)) === Math.abs((Math.abs(circleOne.yCoordinate) - Math.abs(circleTwo.yCoordinate)))){
+    }else if(circleOneX === circleTwoX && (radiusOne + radiusTwo) === Math.abs((Math.abs(circleOneY) - Math.abs(circleTwoY)))){
         circleTangency = true;
         context.beginPath()
         context.strokeStyle = 'blue'
@@ -34,22 +41,18 @@ export const CircleTangencyCheck = (circleOne, circleTwo, context, smallerCircle
 
         let x2 = Number(smallerCircle.yCoordinate) - Number(smallerCircle.radius)
         let x1 = Number(smallerCircle.yCoordinate) + Number(smallerCircle.radius)
-        let govno = 1;
+        extensionCoeff = 1;
 
-        if(Math.sqrt((x2 - Number(largerCircle.yCoordinate))**2 + (0 - Number(largerCircle.xCoordinate)) ** 2) >= Math.sqrt((x1 - Number(largerCircle.yCoordinate))**2 + (0 - Number(largerCircle.xCoordinate)) ** 2)){
-            console.log("Вниз")
-            console.log("Координата меньшей окружности:", smallerCircle)
-            govno = 1;
+        if(Math.sqrt((x2 - largerCircleY)**2 + (0 - largerCircleX) ** 2) >= Math.sqrt((x1 - largerCircleY)**2 + (0 - largerCircleX) ** 2)){
+            extensionCoeff = 1;
 
         }
         else{
-            console.log("Вверх")
-            console.log("Координата меньшей окружности:", smallerCircle)
-            govno = -1
+            extensionCoeff = -1
         }
 
-        context.moveTo((Number(smallerCircle.yCoordinate) + Number(smallerCircle.radius) * govno) * 10, (Number(smallerCircle.xCoordinate) + 10) * 10)
-        context.lineTo((Number(smallerCircle.yCoordinate) + Number(smallerCircle.radius) * govno) * 10, (Number(smallerCircle.xCoordinate) - 10) * 10)
+        context.moveTo((smallerCircleY + smallerCircleRadius * extensionCoeff) * 10, (smallerCircleX + 10) * 10)
+        context.lineTo((smallerCircleY + smallerCircleRadius * extensionCoeff) * 10, (smallerCircleX - 10) * 10)
         context.stroke()
     }
 
--- src/functionsForTangents/CreateTangent.js
+++ src/functionsForTangents/CreateTangent.js
@@ -24,13 +24,6 @@ export const CreateTangent = (context, circleOne, circleTwo) =>{
         const firstTouchPoint = FirstTouchPoint(largerCircle, auxiliaryPoint[0], auxiliaryPoint[1], smallerCircle)
         const secondTouchPoint = SecondTouchPoint(auxiliaryPoint[0], auxiliaryPoint[1], smallerCircle, firstTouchPoint[0], firstTouchPoint[1])
 
-        console.log("Центр отрезка", SearchMiddleSegmentAndRadius(circleOne, circleTwo))
-        console.log("Вспомогательная точка", auxiliaryPoint)
-        console.log("Первая точка касания", firstTouchPoint)
-        console.log("Вторая точка касания", secondTouchPoint)
-
-        console.log(firstTouchPoint[0] === secondTouchPoint[0] && firstTouchPoint[1] === secondTouchPoint[1])
-
         context.beginPath()
 
         context.strokeStyle = 'red'
@@ -41,10 +34,6 @@ export const CreateTangent = (context, circleOne, circleTwo) =>{
 
         let yFirstTouchPoint = k*(firstTouchPoint[0] + TangentExtension(firstTouchPoint, secondTouchPoint)[0]) + b
         let ySecondTouchPoint = k*(secondTouchPoint[0] + TangentExtension(firstTouchPoint, secondTouchPoint)[1]) + b
-
-        console.log("Координаты точек касания: ", firstTouchPoint[0], firstTouchPoint[1], secondTouchPoint[0], secondTouchPoint[1])
-        console.log("kx + b", k, b)
-        console.log("Координаты точек продления: ", yFirstTouchPoint, firstTouchPoint[0] + 10)
 
         context.moveTo(yFirstTouchPoint * 10, (firstTouchPoint[0] + TangentExtension(firstTouchPoint, secondTouchPoint)[0])*10)
         context.lineTo(ySecondTouchPoint*10, (secondTouchPoint[0] + TangentExtension(firstTouchPoint, secondTouchPoint)[1])*10)
