20cd4fc19638ce1fdd5732c28ced425d566bceb6
Михаил Жегалин <mixal.zhegalin@mail.ru>
2023-03-06 23:16:05 +0300

    Вырезал остатки 1 лабы

--- src/LaboratoryTask.js
+++ src/LaboratoryTask.js
@@ -1,16 +1,7 @@
-import {circle} from "./geometry/Сircle";
-import {FoolTest} from "./secondaryFunctions/FoolTest";
-import {OverlayCheck} from "./secondaryFunctions/OverlayCheck";
-import {ExceedingCanvasCheck} from "./secondaryFunctions/ExceedingCanvasCheck ";
 import {bezierCurveEquation} from "./bezierСurves/BezierCurveEquation";
 
-export const LaboratoryTask = (context, circleOne, circleTwo) =>{
-
+export const LaboratoryTask = (context) =>{
 
     bezierCurveEquation([5, 25, 5, 20, 5], [0, 16, 24, 0, 5], context)
-
-    // if(FoolTest(circleOne, circleTwo) && OverlayCheck(circleOne, circleTwo) && ExceedingCanvasCheck(circleOne, circleTwo)){
-    //
-    // }
 
 }
